d5ebc8213569396a379cca06f16634823e124b9d
Paula <paulafffl@gmail.com>
2020-03-24 19:04:24 +0000

    Added middleware for handling CORS requests from index.html

--- server.js
+++ server.js
@@ -8,8 +8,20 @@ module.exports = app;
 */
 const PORT = process.env.PORT || 4001;
 
-// Add middleware for handling CORS requests from index.html
-
+// Added middleware for handling CORS requests from index.html
+var express = require('express');
+var cors = require('cors');
+var app = express();
+ 
+app.use(cors());
+ 
+app.get('/hello/:id', function (req, res, next) {
+  res.json({msg: 'Hello world, we are CORS-enabled!'});
+});
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server is listening on port 80');
+});
 
 // Add middware for parsing request bodies here:
 
