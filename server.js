4bd6b26c5d822b31075aa9ee3844d9c9df35dd34
Paula <paulafffl@gmail.com>
2020-03-24 19:06:43 +0000

    Added code to start the server listening at PORT

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
-  // Add your code to start the server listening at PORT below:
-
+  // Added code to start the server listening at PORT below:
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  })
 }
